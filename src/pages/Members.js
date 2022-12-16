6e9c2f6ee3fbc3d35a0fbdc82711c636110e0dcc
choi <bcaca0831@gmail.com>
2022-12-16 12:12:18 +0900

    useSearchParams / useNavigate / useLocation 사용

--- src/pages/Members.js
+++ src/pages/Members.js
@@ -1,12 +1,31 @@
 import React from "react";
+import { useNavigate } from "react-router";
 
 const Members = (props) => {
+  // useNavigate Hook 은 navigate() 를 리턴한다.
+  // navigate(to, option) 이 있어요.
+  //   to      :  이동할 경로
+  //   option  :  { replace : true/false, state: {} }
+  //             replace 란 history 관리
+  //                     false 가 기본값
+  //                        (이전 history 교체 안함, history 적립중)
+  //                     true  (이전 history 교체해버림)
+
+  //             state 란 네비게이션할 때 전달할 정보
+  //                   이동한 후~~~ 에   location.state 로 사용가능
+
+  const navigate = useNavigate();
+  const goHome = () => {
+    if (window.confirm("홈으로 이동하시겠습니까?")) {
+      navigate("/", { state: { from: "/member" } });
+    }
+  };
+
   // 이미지 사이즈 객체 설정
-  const imgSize = { width: 90, heigth: 80 };
+  const imgSize = { width: 90, height: 80 };
   const list = props.members.map((item, index) => {
     return (
-      // 반복문에서는 key속성이 있어야 하며 unique값 
-      // 버추얼돔의 최적화를 위해 들어가야함
+      // 반복문에서는  key 속성이 있어야하며, unique 값
       <div className="col-6 col-md-4 col-lg-3" key={index}>
         <img
           src={item.photo}
@@ -21,13 +40,17 @@ const Members = (props) => {
       </div>
     );
   });
-  
+
   return (
     <div className="card card-body">
       <h2>Members</h2>
       <div className="container">
         <div className="row">{list}</div>
       </div>
+      <button className="btn btn-primary" onClick={goHome}>
+        {" "}
+        go home{" "}
+      </button>
     </div>
   );
 };
